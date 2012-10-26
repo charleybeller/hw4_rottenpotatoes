6a1ef46a485da691ad5e20bc58464e1d16746b98
charleybeller <charleybeller@gmail.com>
2012-10-26 20:18:29 +0000

    init

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -22,6 +22,10 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /^the edit page for "(.+)"/
+      movie = Movie.find_by_title($1)
+      edit_movie_path(movie)
+
     else
       begin
         page_name =~ /^the (.*) page$/
